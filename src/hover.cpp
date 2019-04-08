bcbc2acdacb61399f386a0bc37ef491f0b51c2ee
Roland Arsenault <roland@ccom.unh.edu>
2019-04-08 15:01:03 -0400

    Added sending of visual feedback.

--- src/hover.cpp
+++ src/hover.cpp
@@ -6,6 +6,7 @@
 #include "marine_msgs/NavEulerStamped.h"
 #include "geometry_msgs/TwistStamped.h"
 #include "geographic_msgs/GeoPointStamped.h"
+#include "geographic_visualization_msgs/GeoVizItem.h"
 
 class Hover
 {
@@ -15,6 +16,7 @@ public:
     {
         m_desired_heading_pub = m_node_handle.advertise<marine_msgs::NavEulerStamped>("/project11/desired_heading",1);
         m_desired_speed_pub = m_node_handle.advertise<geometry_msgs::TwistStamped>("/project11/desired_speed",1);
+        m_display_pub = m_node_handle.advertise<geographic_visualization_msgs::GeoVizItem>("/project11/display",5);
         
         m_position_sub = m_node_handle.subscribe("/position", 10, &Hover::positionCallback, this);
 
@@ -36,10 +38,66 @@ public:
         m_minimum_distance = goal->minimum_distance;
         m_maximum_distance = goal->maximum_distance;
         m_maximum_speed = goal->maximum_speed;
+        
+        geographic_visualization_msgs::GeoVizItem vizItem;
+        vizItem.id = "hover";
+        
+        geographic_visualization_msgs::GeoVizPointList plist;
+        plist.points.push_back(goal->target);
+        plist.size = 10;
+        plist.color.r = .5;
+        plist.color.g = .8;
+        plist.color.b = .5;
+        plist.color.a = 1.0;
+        vizItem.point_groups.push_back(plist);
+        
+        gz4d::geo::Point<double,gz4d::geo::WGS84::LatLon> target_point(goal->target.latitude, goal->target.longitude, 0.0);
+        geographic_visualization_msgs::GeoVizPolygon polygon;
+        // exterior ring is counter-clockwise
+        for (double azimuth = 360.0; azimuth >= 0.0;  azimuth -= 10.0)
+        {
+            auto p = gz4d::geo::WGS84::Ellipsoid::direct(target_point,azimuth,m_maximum_distance);
+            geographic_msgs::GeoPoint gp;
+            gp.latitude = p[0];
+            gp.longitude = p[1];
+            polygon.outer.points.push_back(gp);
+        }
+        
+        geographic_visualization_msgs::GeoVizSimplePolygon inner;
+        // inner ring is clockwise
+        for (double azimuth = 0.0; azimuth <= 360.0;  azimuth += 10.0)
+        {
+            auto p = gz4d::geo::WGS84::Ellipsoid::direct(target_point,azimuth,m_minimum_distance);
+            geographic_msgs::GeoPoint gp;
+            gp.latitude = p[0];
+            gp.longitude = p[1];
+            inner.points.push_back(gp);
+        }
+        polygon.inner.push_back(inner);
+        
+        polygon.fill_color.r = 0.0;
+        polygon.fill_color.g = 1.0;
+        polygon.fill_color.b = 0.0;
+        polygon.fill_color.a = 0.5;
+        
+        polygon.edge_size = 2.0;
+        
+        polygon.edge_color.r = 0.0;
+        polygon.edge_color.g = 0.0;
+        polygon.edge_color.b = 1.0;
+        polygon.edge_color.a = 0.75;
+        
+        vizItem.polygons.push_back(polygon);
+        m_display_pub.publish(vizItem);
     }
     
     void preemptCallback()
     {
+        // send an empty item to display to erase visual feedback
+        geographic_visualization_msgs::GeoVizItem vizItem;
+        vizItem.id = "hover";
+        m_display_pub.publish(vizItem);
+
         m_action_server.setPreempted();
     }
     
@@ -88,6 +146,7 @@ private:
     
     ros::Publisher m_desired_speed_pub;
     ros::Publisher m_desired_heading_pub;
+    ros::Publisher m_display_pub;
     ros::Subscriber m_position_sub;
 
     // goal variables
