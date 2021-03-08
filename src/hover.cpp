46dd17e5a0beba25d602267b78e9310bc2edf9db
Roland Arsenault <roland@ccom.unh.edu>
2021-03-08 14:49:27 -0500

    Use tf2 to calculate distance and bearing to goal

--- src/hover.cpp
+++ src/hover.cpp
@@ -1,41 +1,38 @@
 #include "ros/ros.h"
 
-#include "project11/utils.h"
 #include "hover/hoverAction.h"
 #include "actionlib/server/simple_action_server.h"
-#include "marine_msgs/NavEulerStamped.h"
 #include "geometry_msgs/TwistStamped.h"
 #include "geographic_msgs/GeoPointStamped.h"
 #include "geographic_visualization_msgs/GeoVizItem.h"
-#include "std_msgs/String.h"
 #include "dynamic_reconfigure/server.h"
 #include "hover/hoverConfig.h"
+#include "project11/utils.h"
+#include "project11_transformations/local_services.h"
 
 namespace p11 = project11;
 
 class Hover
 {
 public:
-    Hover(std::string const &name):
-        m_action_server(m_node_handle, name, false),m_autonomous_state(false)
+  Hover(std::string const &name):
+    m_action_server(m_node_handle, name, false)
     {
-        m_desired_heading_pub = m_node_handle.advertise<marine_msgs::NavEulerStamped>("project11/desired_heading",1);
-        m_desired_speed_pub = m_node_handle.advertise<geometry_msgs::TwistStamped>("project11/desired_speed",1);
+        m_cmd_vel_pub = m_node_handle.advertise<geometry_msgs::TwistStamped>("cmd_vel",1);
         m_display_pub = m_node_handle.advertise<geographic_visualization_msgs::GeoVizItem>("project11/display",5);
         
-        m_position_sub = m_node_handle.subscribe("position", 10, &Hover::positionCallback, this);
-        m_heading_sub = m_node_handle.subscribe("heading",10, &Hover::headingCallback, this);
+        ros::NodeHandle nh_private("~");
+    
+        nh_private.param<std::string>("map_frame", m_map_frame, "map");
+        nh_private.param<std::string>("base_frame", m_base_frame, "base_link");
 
-        m_state_sub = m_node_handle.subscribe("project11/piloting_mode", 10, &Hover::stateCallback, this);
-
-        dynamic_reconfigure::Server<hover::hoverConfig>::CallbackType f;
-        f = boost::bind(&Hover::reconfigureCallback, this,  _1, _2);
-        m_config_server.setCallback(f);
-
-        
-        m_action_server.registerGoalCallback(boost::bind(&Hover::goalCallback, this));
-        m_action_server.registerPreemptCallback(boost::bind(&Hover::preemptCallback, this));
+        m_config_server.setCallback(std::bind(&Hover::reconfigureCallback, this,  std::placeholders::_1, std::placeholders::_2));
+          
+        m_action_server.registerGoalCallback(std::bind(&Hover::goalCallback, this));
+        m_action_server.registerPreemptCallback(std::bind(&Hover::preemptCallback, this));
         m_action_server.start();
+        
+        m_timer = m_node_handle.createTimer(ros::Duration(0.1), std::bind(&Hover::timerCallback, this, std::placeholders::_1));
     }
     
     ~Hover()
@@ -45,9 +42,8 @@ public:
     void goalCallback()
     {
         auto goal = m_action_server.acceptNewGoal();
-        
-        m_target[0] = goal->target.latitude;
-        m_target[1] = goal->target.longitude;
+        p11::fromMsg(goal->target, m_target);
+        m_target_map = m_transforms.wgs84_to_map(goal->target,m_map_frame);
         sendDisplay();
     }
     
@@ -59,34 +55,22 @@ public:
         {
             geographic_visualization_msgs::GeoVizPointList plist;
             geographic_msgs::GeoPoint gp;
-            gp.latitude = m_target[0];
-            gp.longitude = m_target[1];
+            p11::toMsg(m_target, gp);
             plist.points.push_back(gp);
             plist.size = 10;
-            if (m_autonomous_state)
-            {
-                plist.color.r = .5;
-                plist.color.g = .8;
-                plist.color.b = .5;
-                plist.color.a = 1.0;
-            }
-            else
-            {
-                plist.color.r = .2;
-                plist.color.g = .3;
-                plist.color.b = .2;
-                plist.color.a = .5;
-            }    
+            plist.color.r = .5;
+            plist.color.g = .8;
+            plist.color.b = .5;
+            plist.color.a = 1.0;
             vizItem.point_groups.push_back(plist);
             
             geographic_visualization_msgs::GeoVizPolygon polygon;
             // exterior ring is counter-clockwise
             for (double azimuth = 360.0; azimuth >= 0.0;  azimuth -= 10.0)
             {
-                auto p = gz4d::geo::WGS84::Ellipsoid::direct(m_target,azimuth,m_maximum_distance);
+                p11::LatLongDegrees p = p11::WGS84::direct(m_target, p11::AngleDegrees(azimuth), m_maximum_distance);
                 geographic_msgs::GeoPoint gp;
-                gp.latitude = p[0];
-                gp.longitude = p[1];
+                p11::toMsg(p, gp);
                 polygon.outer.points.push_back(gp);
             }
             
@@ -94,45 +78,24 @@ public:
             // inner ring is clockwise
             for (double azimuth = 0.0; azimuth <= 360.0;  azimuth += 10.0)
             {
-                auto p = gz4d::geo::WGS84::Ellipsoid::direct(m_target,azimuth,m_minimum_distance);
+                p11::LatLongDegrees p = p11::WGS84::direct(m_target, p11::AngleDegrees(azimuth), m_minimum_distance);
                 geographic_msgs::GeoPoint gp;
-                gp.latitude = p[0];
-                gp.longitude = p[1];
+                p11::toMsg(p, gp);
                 inner.points.push_back(gp);
             }
             polygon.inner.push_back(inner);
             
-            if (m_autonomous_state)
-            {
-                polygon.fill_color.r = 0.0;
-                polygon.fill_color.g = 1.0;
-                polygon.fill_color.b = 0.0;
-                polygon.fill_color.a = 0.5;
-            }
-            else
-            {
-                polygon.fill_color.r = 0.0;
-                polygon.fill_color.g = 0.5;
-                polygon.fill_color.b = 0.0;
-                polygon.fill_color.a = 0.25;
-            }
+            polygon.fill_color.r = 0.0;
+            polygon.fill_color.g = 1.0;
+            polygon.fill_color.b = 0.0;
+            polygon.fill_color.a = 0.5;
             
             polygon.edge_size = 2.0;
             
-            if (m_autonomous_state)
-            {
-                polygon.edge_color.r = 0.0;
-                polygon.edge_color.g = 0.0;
-                polygon.edge_color.b = 1.0;
-                polygon.edge_color.a = 0.75;
-            }
-            else
-            {
-                polygon.edge_color.r = 0.0;
-                polygon.edge_color.g = 0.0;
-                polygon.edge_color.b = 0.5;
-                polygon.edge_color.a = 0.375;
-            }
+            polygon.edge_color.r = 0.0;
+            polygon.edge_color.g = 0.0;
+            polygon.edge_color.b = 1.0;
+            polygon.edge_color.a = 0.75;
             
             vizItem.polygons.push_back(polygon);
         }
@@ -145,58 +108,52 @@ public:
         sendDisplay();
     }
 
-    void headingCallback(const marine_msgs::NavEulerStamped::ConstPtr& msg)
-    {
-        m_heading = msg->orientation.heading;
-    }
 
-    void positionCallback(const geographic_msgs::GeoPointStamped::ConstPtr& inmsg)
+    void timerCallback(const ros::TimerEvent event)
     {
-        if(m_action_server.isActive() && m_autonomous_state)
+        if(m_action_server.isActive())
         {
-          p11::LatLongDegrees vehicle_position;
-          p11::fromMsg(inmsg->position, vehicle_position);
-          auto azimuth_distance_to_target = p11::WGS84::inverse(vehicle_position, m_target);
-            
-          float target_speed = 0.0;
-          float range = azimuth_distance_to_target.second;
-          if (range >= m_maximum_distance)
-              target_speed = m_maximum_speed;
-          else if (range > m_minimum_distance)
+          try
           {
-              float p = (range-m_minimum_distance)/(m_maximum_distance-m_minimum_distance);
-              target_speed = p*m_maximum_speed;
+            auto t = m_transforms().lookupTransform(m_base_frame, m_map_frame, ros::Time(0));
+            
+            geometry_msgs::Point target_local;
+            tf2::doTransform(m_target_map,target_local,t);
+
+            double range = sqrt(target_local.x*target_local.x+target_local.y*target_local.y);
+            p11::AngleRadiansZeroCentered bearing(atan2(target_local.y, target_local.x));
+            
+            float target_speed = 0.0;
+            if (range >= m_maximum_distance)
+                target_speed = m_maximum_speed;
+            else if (range > m_minimum_distance)
+            {
+                float p = (range-m_minimum_distance)/(m_maximum_distance-m_minimum_distance);
+                target_speed = p*m_maximum_speed;
+            }
+              
+            hover::hoverFeedback feedback;
+            feedback.range = range;
+            feedback.bearing = p11::AngleDegrees(bearing).value();
+            feedback.speed = target_speed;
+              
+            m_action_server.publishFeedback(feedback);
+              
+            ros::Time now = ros::Time::now();
+            
+            geometry_msgs::TwistStamped cmd_vel;
+            cmd_vel.header.stamp = now;
+            cmd_vel.twist.angular.z = bearing.value();
+            cmd_vel.twist.linear.x = target_speed;
+            m_cmd_vel_pub.publish(cmd_vel);
           }
-            
-          p11::AngleDegrees bearing(azimuth_distance_to_target.first);
-          
-          hover::hoverFeedback feedback;
-          feedback.range = azimuth_distance_to_target.second;
-          feedback.bearing = bearing.value();
-          feedback.speed = target_speed;
-            
-          m_action_server.publishFeedback(feedback);
-            
-          ros::Time now = ros::Time::now();
-            
-          marine_msgs::NavEulerStamped desired_heading;
-          desired_heading.header.stamp = now;
-          desired_heading.orientation.heading = bearing.value();
-          m_desired_heading_pub.publish(desired_heading);
-            
-          geometry_msgs::TwistStamped desired_speed;
-          desired_speed.header.stamp = now;
-          desired_speed.twist.linear.x = target_speed;
-          m_desired_speed_pub.publish(desired_speed);
+          catch (tf2::TransformException &ex)
+          {
+            ROS_WARN_STREAM("Hover: " << ex.what());
+          }
         }
     }
     
-    void stateCallback(const std_msgs::String::ConstPtr &inmsg)
-    {
-        m_autonomous_state = inmsg->data == "autonomous";
-        sendDisplay();
-    }
-
     void reconfigureCallback(hover::hoverConfig &config, uint32_t level)
     {
         m_minimum_distance = config.minimum_distance;
@@ -210,24 +167,25 @@ private:
     actionlib::SimpleActionServer<hover::hoverAction> m_action_server;
 
     
-    ros::Publisher m_desired_speed_pub;
-    ros::Publisher m_desired_heading_pub;
+    ros::Publisher m_cmd_vel_pub;
     ros::Publisher m_display_pub;
-    ros::Subscriber m_position_sub;
-    ros::Subscriber m_heading_sub;
-    ros::Subscriber m_state_sub;
     
     dynamic_reconfigure::Server<hover::hoverConfig> m_config_server;
 
     // goal variables
     p11::LatLongDegrees m_target;
+    geometry_msgs::Point m_target_map;
     
     float m_minimum_distance; // meters
     float m_maximum_distance; // meters
     float m_maximum_speed;    // m/s
-    bool m_autonomous_state;
-    double m_heading;
     
+    p11::Transformations m_transforms;
+    
+    std::string m_map_frame;
+    std::string m_base_frame;
+    
+    ros::Timer m_timer;
 };
 
 int main(int argc, char **argv)
